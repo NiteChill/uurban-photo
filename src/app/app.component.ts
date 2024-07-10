e90850da0911c4b487063fb6a13001304d123f75
puissantachille <puissantachille@gmail.com>
2024-07-10 22:22:00 +0000

    Design almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardPhotoComponent } from './components/card-photo/card-photo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardPhotoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/card-photo/card-photo.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-photo',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-photo.component.html',
+  styleUrl: './card-photo.component.scss'
+})
+export class CardPhotoComponent {
+  @Input() src: string = '';
+
+}
