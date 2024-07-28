b5c0b3c4c0d2d484731d0234e943de272d6b3929
puissantachille <puissantachille@gmail.com>
2024-07-28 13:37:57 +0000

    animation timings

--- src/app/modalPhotoAnimation.ts
+++ src/app/modalPhotoAnimation.ts
@@ -4,14 +4,14 @@ export const modalPhotoAnimation = trigger('modalPhotoAnimation', [
   transition(':enter', [
     style({ opacity: 0, width: '90vw' }),
     animate(
-      '0.3s cubic-bezier(0.7, 0, 0.8, 0.15)',
+      '0.2s cubic-bezier(0.7, 0, 0.8, 0.15)',
       style({ opacity: 1, width: '100vw' })
     ),
   ]),
   transition(':leave', [
     style({ opacity: 1, width: '100vw' }),
     animate(
-      '0.3s cubic-bezier(0.7, 0, 0.8, 0.15)',
+      '0.2s cubic-bezier(0.7, 0, 0.8, 0.15)',
       style({ opacity: 0, width: '90vw' })
     ),
   ]),
