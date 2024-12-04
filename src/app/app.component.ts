ba0ca35237444d3606a5d4f67758380278db9e2e
puissantachille <puissantachille@gmail.com>
2024-12-04 20:49:44 +0000

    split

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { UurbanAndFriends01Component } from './pages/uurban-and-friends-01/uurban-and-friends-01.component';
-import { UurbanClub01Component } from './pages/uurban-club-01/uurban-club-01.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ UurbanAndFriends01Component, UurbanClub01Component],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
