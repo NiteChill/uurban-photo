54f1dd2b3ff62c57bd833cce4a12144e60ad466d
puissantachille <puissantachille@gmail.com>
2024-12-04 19:18:53 +0000

    one page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UurbanAndFriends01Component } from './pages/uurban-and-friends-01/uurban-and-friends-01.component';
+import { UurbanClub01Component } from './pages/uurban-club-01/uurban-club-01.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ UurbanAndFriends01Component, UurbanClub01Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  animations: [],
 })
 export class AppComponent {
   title = 'myapp';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,3 @@
 import { Routes } from '@angular/router';
-import { UurbanAndFriends01Component } from './pages/uurban-and-friends-01/uurban-and-friends-01.component';
-import { UurbanClub01Component } from './pages/uurban-club-01/uurban-club-01.component';
 
-export const routes: Routes = [
-  {
-    path: 'UurbanAndFriends01',
-    component: UurbanAndFriends01Component,
-  },
-  {
-    path: 'UurbanClub01',
-    component: UurbanClub01Component,
-  },
-  { path: '**', redirectTo: 'UurbanClub01', pathMatch: 'full' },
-];
+export const routes: Routes = [];
